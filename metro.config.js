4e66fde91a1840a55efe2fe62e12ab7cbdb11577
Kartik Vyas <vkartik013@gmail.com>
2026-03-14 13:31:49 +0530

    fix: update Metro configuration to include node libraries and modify iOS run script

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,15 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const nodeLibs = require('node-libs-react-native');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
 module.exports = mergeConfig(defaultConfig, {
   resolver: {
     extraNodeModules: {
+      ...nodeLibs,
       crypto: require.resolve('react-native-quick-crypto'),
       buffer: require.resolve('buffer'),
       process: require.resolve('process/browser'),
-      stream: require.resolve('stream-browserify'),
-      string_decoder: require.resolve('string_decoder'),
-      events: require.resolve('events'),
-      util: require.resolve('util'),
-      vm: require.resolve('vm-browserify'),
     },
   },
 });
