ba8bbfa8d8443319104f98f6c124e51541e272c7
Kartik Vyas <vkartik013@gmail.com>
2026-03-14 13:30:07 +0530

    chore: update dependencies in package.json
    - Added new dependencies: events, node-libs-react-native, process, stream-browserify, string_decoder, util, vm-browserify
    - Updated devDependencies: @react-native/babel-preset, @react-native/metro-config

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
+import { install } from 'react-native-quick-crypto';
+import { Buffer } from 'buffer';
+import process from 'process';
 import App from './App';
 import { name as appName } from './app.json';
+
+install();
+
+global.Buffer = Buffer;
+global.process = process;
 
 AppRegistry.registerComponent(appName, () => App);
--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,14 @@ const defaultConfig = getDefaultConfig(__dirname);
 module.exports = mergeConfig(defaultConfig, {
   resolver: {
     extraNodeModules: {
+      crypto: require.resolve('react-native-quick-crypto'),
       buffer: require.resolve('buffer'),
+      process: require.resolve('process/browser'),
+      stream: require.resolve('stream-browserify'),
+      string_decoder: require.resolve('string_decoder'),
+      events: require.resolve('events'),
+      util: require.resolve('util'),
+      vm: require.resolve('vm-browserify'),
     },
   },
 });
